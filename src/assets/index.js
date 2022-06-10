f3a82a4857b530f3ce191a2dc0ae07035c4e4070
ajimiftahussalim <ajimiftahussalim288@gmail.com>
2022-06-10 23:31:11 +0700

    Update folder public + package.json, package-lock.json + index.js, index.css (folder src) folder assets di src/assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,44 +1,14 @@
-import RegisterBg from './images/register-bg.jpg';
-import LoginBg from './images/login-bg.jpg'
 import ImgJumbotron from './images/img_jumbotron.png';
 import Logo from './images/logo.png';
-//icons
-import ICFacebook from './icons/facebook.png';
-import ICTwitter from './icons/twitter.png';
-import ICInstagram from './icons/instagram.png';
-import ICTelegram from './icons/telegram.png';
-import ICDiscord from './icons/discord.png';
-import ICGithub from './icons/github.png';
-// category
-import WisataAlam from './images/category/wisata alam.jpg';
-import WisataPantai from './images/category/wisata pantai.jpeg';
-import WisataRekreasi from './images/category/wisata rekreasi.jpg';
-import KampungWisata from './images/category/kampung wisata.jpg';
-import WisataBudaya from './images/category/wisata budaya.jpg';
-import WisataEdukasi from './images/category/wisata edukasi.jpg';
 // our team
-import Eko from './images/our team/ekor.jpg';
+import Eko from './images/our team/eko.jpg';
 import Disha from './images/our team/disha.jpg';
 import Faisal from './images/our team/faisal.jpg';
 import Aji from './images/our team/aji.jpg';
 
 export { 
-    RegisterBg,
-    LoginBg,
     Logo,
-    ICFacebook,
-    ICTwitter,
-    ICInstagram,
-    ICTelegram,
-    ICDiscord,
-    ICGithub,
     ImgJumbotron,
-    WisataAlam,
-    WisataPantai,
-    WisataRekreasi,
-    KampungWisata,
-    WisataBudaya,
-    WisataEdukasi,
     Eko,
     Disha,
     Faisal,
